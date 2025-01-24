78ebddd9c8f50206be4f2b4a1bf7760789563f4a
Wee Sritippho <weeaix@gmail.com>
2025-01-24 22:08:16 +0700

    Added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  ...eslintPluginAstro.configs.recommended,
+);
