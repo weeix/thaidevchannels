8b82d135680031b84b9d1835701b1b5cbc509951
Wee Sritippho <weeaix@gmail.com>
2025-01-24 22:24:24 +0700

    Run prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   env: {
     schema: {
-      SITE_NAME: envField.string({ context: "server", access: "public", default: "ThaiDevChannels" }),
-      SITE_DESCRIPTION: envField.string({ context: "server", access: "public", default: "เว็บรวบรวมช่องของคอนเทนครีเอเตอร์สายเขียนโค้ด" }),
+      SITE_NAME: envField.string({
+        context: "server",
+        access: "public",
+        default: "ThaiDevChannels",
+      }),
+      SITE_DESCRIPTION: envField.string({
+        context: "server",
+        access: "public",
+        default: "เว็บรวบรวมช่องของคอนเทนครีเอเตอร์สายเขียนโค้ด",
+      }),
     },
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 export default tseslint.config(
   eslint.configs.recommended,
